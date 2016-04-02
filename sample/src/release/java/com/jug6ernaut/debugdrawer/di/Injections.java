3353dcdc962e7dcf657af717a688769e10e334f0
williamwebb <jug6ernaut@gmail.com>
2016-04-02 10:00:53 -0500

    Enable java8 with jack and retrolambda for libaries

--- sample/src/release/java/com/jug6ernaut/debugdrawer/di/Injections.java
+++ sample/src/release/java/com/jug6ernaut/debugdrawer/di/Injections.java
@@ -1,9 +1,6 @@
 package com.jug6ernaut.debugdrawer.di;
 
-import android.app.Activity;
 import com.jug6ernaut.debugdrawer.DebugDrawer;
-import com.jug6ernaut.debugdrawer.modules.*;
-import io.jug6ernaut.debugdrawer.modules.TimberModule;
 
 /**
  * Created by williamwebb on 4/2/16.
